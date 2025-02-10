311871856a4259d992466bfe5cc2ad520e47aee3
Muhammad Ilman Aqilaa <ilmanaqilaa2@gmail.com>
2025-02-10 15:19:30 +0700

    feat(cuyuniversity): :sparkles: [sesi1/latihan.py] Enhance mini-game with user input validation and welcome message

--- cuyuniversity/sesi1/latihan.py
+++ cuyuniversity/sesi1/latihan.py
@@ -1,35 +1,47 @@
 import random
+from libs import welcome_message
 
-welcome_massage  = "SELAMAT DATANG DI MINIGAME"
 marmut_position = random.randint(1, 4)
 
-print("********************************")
-print(f"** {welcome_massage} **")
-print("********************************")
+welcome_message("Wellcome to Minigame")
+
+print()
 
 nama_user = input("Siapa namamu: ")
+
+while nama_user == "":
+    nama_user = input("Jangan kosong dongg, Ayo siapa namamu?")
 
 goa_form = "|_|"
 empty_goa = [goa_form] * 4
 
-string_empty_goa = " ".join(empty_goa)
 
 goa = empty_goa.copy()
 
 goa[marmut_position - 1] = "[0_0]"
-string_goa = " ".join(goa)
+
+empty_goa = " ".join(empty_goa)
+goa = " ".join(goa)
 
 print(f"""
 Halo {nama_user}! Coba perhatikan goa di bawah ini
-{string_empty_goa}
+{empty_goa}
 """)
 
 pilihan_user = int(input("Menurutmu di goa nomor berapa marmut berada? [1 / 2 / 3 / 4]: "))
 
-konfirmasi = input("Apakah kamu sudah yakin memilih nomor goa tersebut? [Y / N]")
+while pilihan_user not in [1, 2, 3, 4]:
+    pilihan_user = int(input("Angkamu tidak sesuai! Ayo pilih angka yang benar!!! [1 / 2 / 3 / 4]: "))
+    
+print()
+
+konfirmasi = input("Apakah kamu sudah yakin memilih nomor goa tersebut? [Y / N] ")
+
+print()
 
 if konfirmasi in ["Y", "y"]:
     print(f"Pilihanmu adalah goa bernomor {pilihan_user}")
+    print()
 elif konfirmasi in ["N", "n"]:
     pilihan_user = int(input("Menurutmu di goa nomor berapa marmut berada? [1 / 2 / 3 / 4]: "))
     print(f"Pilihanmu adalah goa bernomor {pilihan_user}")
@@ -38,8 +50,8 @@ else:
     exit()
 
 if pilihan_user == marmut_position:
-    print(string_goa)
+    print(goa)
     print(f"Selamat {nama_user} kamu benar! Posisi marmut berada di goa nomor {marmut_position} dan pilihanmu adalah goa nomor {pilihan_user}")
 else:
-    print(string_goa)
+    print(goa)
     print(f"Kamu salah!!! Posisi marmut bukan berada di goa {pilihan_user}, tapi berada di goa {marmut_position}")
---
+++ cuyuniversity/sesi1/libs.py
@@ -0,0 +1,5 @@
+def welcome_message(title):
+    star = "*" * (len(title) + 8)
+    print(star)
+    print(f"*** {title} ***")
+    print(star)
