0af5abad10cf5659bfd5093da1df36aa07210b10
Muhammad Ilman Aqilaa <ilmanaqilaa2@gmail.com>
2025-02-10 09:28:14 +0700

    feat(cuyuniversity): :sparkles: [sesi1] Update game logic to display empty and filled goa states

--- cuyuniversity/sesi1/latihan.py
+++ cuyuniversity/sesi1/latihan.py
@@ -9,10 +9,19 @@ print("********************************")
 
 nama_user = input("Siapa namamu: ")
 
+goa_form = "|_|"
+empty_goa = [goa_form] * 4
+
+string_empty_goa = " ".join(empty_goa)
+
+goa = empty_goa.copy()
+
+goa[marmut_position - 1] = "[0_0]"
+string_goa = " ".join(goa)
+
 print(f"""
 Halo {nama_user}! Coba perhatikan goa di bawah ini
-
-|_| |_| |_| |_|     
+{string_empty_goa}
 """)
 
 pilihan_user = int(input("Menurutmu di goa nomor berapa marmut berada? [1 / 2 / 3 / 4]: "))
@@ -29,6 +38,8 @@ else:
     exit()
 
 if pilihan_user == marmut_position:
+    print(string_goa)
     print(f"Selamat {nama_user} kamu benar! Posisi marmut berada di goa nomor {marmut_position} dan pilihanmu adalah goa nomor {pilihan_user}")
 else:
+    print(string_goa)
     print(f"Kamu salah!!! Posisi marmut bukan berada di goa {pilihan_user}, tapi berada di goa {marmut_position}")
