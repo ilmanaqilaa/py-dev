c8272ec6306160ad519ea744f224ec1cb9b86960
Muhammad Ilman Aqilaa <ilmanaqilaa2@gmail.com>
2025-02-12 20:43:44 +0700

    feat(cuyuniversity): :sparkles: Menambahkan opsi keluar ke menu utama untuk meningkatkan navigasi program

--- cuyuniversity/sesi1/main.py
+++ cuyuniversity/sesi1/main.py
@@ -3,19 +3,20 @@ from games import cuypy
 from tools import warung_mini
 
 def menu():
-    user_option = int(input(f"Menu program: \n1. Games Cuypy\n2. Warung Mini\n\nsilahkan pilih: "))
+    user_option = int(input(f"Menu program: \n1. Games Cuypy\n2. Warung Mini\n3. Keluar Program\n\nsilahkan pilih: "))
     
     if user_option == 1:
         cuypy.start_cuypy()
     elif user_option == 2:
         warung_mini.start_warung()
+    elif user_option == 3:
+        exit_program()
     else:
         print("Pilih lah program yang tersedia")
 
 def main():
     welcome_message()
     menu()
-    exit_program()
 
 if __name__ == '__main__':
     main()
