d2825eb12817be635233cc17ac336221c33b67e7
Muhammad Ilman Aqilaa <ilmanaqilaa2@gmail.com>
2025-02-12 12:01:26 +0700

    feat(cuyuniversity): :sparkles: [sesi1/latihan.py] Simplify user prompts and remove unnecessary confirmation steps in mini-game

--- cuyuniversity/sesi1/latihan.py
+++ cuyuniversity/sesi1/latihan.py
@@ -5,18 +5,10 @@ welcome_message("Wellcome to Minigame")
 
 print()
 
-nama_user = input("Siapa namamu: ")
-
-while nama_user == "":
-    nama_user = input("Jangan kosong dongg, Ayo siapa namamu?")
-
-
-
 while True:
 
     goa_form = "|_|"
     empty_goa = [goa_form] * 4
-
 
     marmut_position = random.randint(1, 4)
     
@@ -25,10 +17,7 @@ while True:
 
     empty_goa = " ".join(empty_goa)
     goa = " ".join(goa)
-    print(f"""
-    Halo {nama_user}! Coba perhatikan goa di bawah ini
-    {empty_goa}
-    """)
+    print(f"Coba perhatikan goa di bawah ini \n\n{empty_goa}\n")
 
     pilihan_user = int(input("Menurutmu di goa nomor berapa marmut berada? [1 / 2 / 3 / 4]: "))
 
@@ -37,23 +26,9 @@ while True:
         
     print()
 
-    konfirmasi = input("Apakah kamu sudah yakin memilih nomor goa tersebut? [Y / N] ")
-
-    print()
-
-    if konfirmasi in ["Y", "y"]:
-        print(f"Pilihanmu adalah goa bernomor {pilihan_user}")
-        print()
-    elif konfirmasi in ["N", "n"]:
-        pilihan_user = int(input("Menurutmu di goa nomor berapa marmut berada? [1 / 2 / 3 / 4]: "))
-        print(f"Pilihanmu adalah goa bernomor {pilihan_user}")
-    else:
-        input("Kamu memasukan konfirmasi yang tidak valid. Ulangi program!!!")
-        exit()
-
     if pilihan_user == marmut_position:
         print(goa)
-        print(f"Selamat {nama_user} kamu benar! Posisi marmut berada di goa nomor {marmut_position} dan pilihanmu adalah goa nomor {pilihan_user}")
+        print(f"Selamat kamu benar! Posisi marmut berada di goa nomor {marmut_position} dan pilihanmu adalah goa nomor {pilihan_user}")
     else:
         print(goa)
         print(f"Kamu salah!!! Posisi marmut bukan berada di goa {pilihan_user}, tapi berada di goa {marmut_position}")
